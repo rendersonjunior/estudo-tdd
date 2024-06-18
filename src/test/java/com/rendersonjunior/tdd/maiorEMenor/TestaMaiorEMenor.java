3d55017f0a11819299ceef604c6b1bb2ac1a2ea5
Renderson Junior <rendersonjuniorcmp@gmail.com>
2024-06-18 00:50:38 -0300

    feat: inclusao de teste de ordem variada e de apenas um produto na lista

--- src/test/java/com/rendersonjunior/tdd/maiorEMenor/TestaMaiorEMenor.java
+++ src/test/java/com/rendersonjunior/tdd/maiorEMenor/TestaMaiorEMenor.java
@@ -1,4 +1,4 @@
-package com.rendersonjunior.java.maiorEMenor;
+package com.rendersonjunior.tdd.maiorEMenor;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -34,7 +34,32 @@ public class TestaMaiorEMenor {
         Assert.assertEquals("Geladeira", algoritmo.getMaior().getNome());
     }
 
-    public static void main(String[] args) {
+    @Test
+    public void ordemVariada() {
+        final var carrinho = new CarrinhoDeCompras();
+
+        carrinho.adiciona(new Produto("Liquidificador", 250.0));
+        carrinho.adiciona(new Produto("Jogo de Pratos", 70.0));
+        carrinho.adiciona(new Produto("Geladeira", 450.0));
+
+        final var algoritmo = new MaiorEMenor();
+        algoritmo.encontra(carrinho);
+
+        Assert.assertEquals("Jogo de Pratos", algoritmo.getMenor().getNome());
+        Assert.assertEquals("Geladeira", algoritmo.getMaior().getNome());
+    }
+
+    @Test
+    public void apenasUmProduto() {
+        final var carrinho = new CarrinhoDeCompras();
+
+        carrinho.adiciona(new Produto("Geladeira", 450.0));
+
+        final var algoritmo = new MaiorEMenor();
+        algoritmo.encontra(carrinho);
+
+        Assert.assertEquals("Geladeira", algoritmo.getMenor().getNome());
+        Assert.assertEquals("Geladeira", algoritmo.getMaior().getNome());
     }
 
 }
