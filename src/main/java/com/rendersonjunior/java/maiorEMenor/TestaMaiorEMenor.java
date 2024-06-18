d7a572638b7dfee9e50825ca0125214f9d3aea81
Renderson Junior <rendersonjuniorcmp@gmail.com>
2024-06-17 23:58:54 -0300

    test: inclusao de teste de ordem crescente e decrescente

--- src/main/java/com/rendersonjunior/java/maiorEMenor/TestaMaiorEMenor.java
+++ src/main/java/com/rendersonjunior/java/maiorEMenor/TestaMaiorEMenor.java
@@ -1,10 +1,14 @@
 package com.rendersonjunior.java.maiorEMenor;
 
+import org.junit.Test;
+
 public class TestaMaiorEMenor {
-    public static void main(String[] args) {
+    @Test
+    public void ordemDecrescente() {
         final var carrinho = new CarrinhoDeCompras();
-        carrinho.adiciona(new Produto("Liquidificador", 250.0));
+
         carrinho.adiciona(new Produto("Geladeira", 450.0));
+        carrinho.adiciona(new Produto("Liquidificador", 250.0));
         carrinho.adiciona(new Produto("Jogo de Pratos", 70.0));
 
         final var algoritmo = new MaiorEMenor();
@@ -12,7 +16,24 @@ public class TestaMaiorEMenor {
 
         System.out.println("O menor produto: ".concat(algoritmo.getMenor().getNome()));
         System.out.println("O maior produto: ".concat(algoritmo.getMaior().getNome()));
+    }
 
+    @Test
+    public void ordemCrescente() {
+        final var carrinho = new CarrinhoDeCompras();
+
+        carrinho.adiciona(new Produto("Jogo de Pratos", 70.0));
+        carrinho.adiciona(new Produto("Liquidificador", 250.0));
+        carrinho.adiciona(new Produto("Geladeira", 450.0));
+
+        final var algoritmo = new MaiorEMenor();
+        algoritmo.encontra(carrinho);
+
+        System.out.println("O menor produto: ".concat(algoritmo.getMenor().getNome()));
+        System.out.println("O maior produto: ".concat(algoritmo.getMaior().getNome()));
+    }
+
+    public static void main(String[] args) {
     }
 
 }
