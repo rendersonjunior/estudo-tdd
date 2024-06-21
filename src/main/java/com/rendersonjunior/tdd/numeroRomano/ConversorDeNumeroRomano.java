fa92b785d2803649489f882fd0da5958e3e85545
Renderson Junior <rendersonjuniorcmp@gmail.com>
2024-06-21 00:04:42 -0300

    feat: alteracao do metodo converte para tratamento caso o numero romano passado tenha mais de um algarismo

--- src/main/java/com/rendersonjunior/tdd/numeroRomano/ConversorDeNumeroRomano.java
+++ src/main/java/com/rendersonjunior/tdd/numeroRomano/ConversorDeNumeroRomano.java
@@ -1,9 +1,28 @@
 package com.rendersonjunior.tdd.numeroRomano;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConversorDeNumeroRomano {
 
-    public int converte(final String numeroEmRomano) {
-        return 0;
+    /*
+    private static Map<String, Integer> tabela = new HashMap<>() {{
+        put("I", 1);
+        put("V", 5);
+        put("X", 10);
+        put("L", 50);
+        put("C", 100);
+        put("D", 500);
+        put("M", 1000);
+    }};
+    */
+
+    public Integer converte(final String numeroEmRomano) {
+        Integer acumulador = 0;
+        for (int i = 0; i < numeroEmRomano.length(); i++) {
+            acumulador += EnumTabelaNumeroRomano.buscarNumeroDecimal(String.valueOf(numeroEmRomano.charAt(i)));
+        }
+        return acumulador;
     }
 
 }
