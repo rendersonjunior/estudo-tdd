888067164a020380400ea15c50d64aa330e3ad77
Renderson Junior <rendersonjuniorcmp@gmail.com>
2024-06-21 00:03:29 -0300

    test: adicao de teste de dois algarismos romanos

--- src/main/java/com/rendersonjunior/tdd/numeroRomano/ConversorDeNumeroRomanoTest.java
+++ src/main/java/com/rendersonjunior/tdd/numeroRomano/ConversorDeNumeroRomanoTest.java
@@ -9,8 +9,22 @@ public class ConversorDeNumeroRomanoTest {
     @Test
     public void deveEntenderOSimboloI() {
         final ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
-        int numero = romano.converte("I");
+        final int numero = romano.converte("I");
         assertEquals(1, numero);
+    }
+
+    @Test
+    public void deveEntenderOSimboloV() {
+        final ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+        final int numero = romano.converte("V");
+        assertEquals(5, numero);
+    }
+
+    @Test
+    public void deveEntenderDoisSimbolosComoII() {
+        final ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+        final int numero = romano.converte("II");
+        assertEquals(2, numero);
     }
 
 }
