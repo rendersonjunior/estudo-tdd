2eaf7cf9d099446e3024a0cbc832be68e40cd5ab
Renderson Junior <rendersonjuniorcmp@gmail.com>
2024-06-27 22:01:26 -0300

    feat: criacao de calculadora de salario

---
+++ src/main/java/com/rendersonjunior/tdd/calculoSalario/CalculadoraDeSalario.java
@@ -0,0 +1,9 @@
+package com.rendersonjunior.tdd.calculoSalario;
+
+public class CalculadoraDeSalario {
+
+    public Double calculaSalario(final Funcionario funcionario) {
+        return 1350.0;
+    }
+
+}
