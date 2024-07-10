11797065831db06b46a0ca7ad22e757431565e31
rendersonjunior <rendersonjuniorcmp@gmail.com>
2024-07-10 00:07:36 -0300

    test: instanciacao do tdd do teste de deveRetornarMaiorValorSeCarrinhoContemMuitosElementos e melhoria no service

--- src/main/java/com/rendersonjunior/tdd/carrinhoDeCompras/service/MaiorPreco.java
+++ src/main/java/com/rendersonjunior/tdd/carrinhoDeCompras/service/MaiorPreco.java
@@ -1,12 +1,14 @@
 package com.rendersonjunior.tdd.carrinhoDeCompras.service;
 
+import com.rendersonjunior.tdd.carrinhoDeCompras.model.Item;
+
 public class MaiorPreco {
 
     public Double encontra(final CarrinhoDeCompras carrinho) {
-        if (carrinho.getItens().size() == 0) {
-            return  0.0;
-        }
-        return carrinho.getItens().get(0).getValorTotal();
+        return carrinho.getItens().stream()
+                .map(Item::getValorTotal)
+                .max(Double::compareTo)
+                .orElse(0.0);
     }
 
 }
--- src/test/java/com/rendersonjunior/tdd/carrinhoDeCompras/CarrinhoDeComprasTest.java
+++ src/test/java/com/rendersonjunior/tdd/carrinhoDeCompras/CarrinhoDeComprasTest.java
@@ -30,4 +30,17 @@ public class CarrinhoDeComprasTest {
         assertEquals(900.0, valor, 0.0001);
     }
 
+    @Test
+    public void deveRetornarMaiorValorSeCarrinhoContemMuitosElementos() {
+        final var carrinho = new CarrinhoDeCompras();
+        carrinho.adiciona(new Item("Geladeira", 1, 900.0));
+        carrinho.adiciona(new Item("Fogão", 1, 1500.0));
+        carrinho.adiciona(new Item("Máquina de Lavar", 1, 750.0));
+
+        final var algoritmo = new MaiorPreco();
+        final var valor = algoritmo.encontra(carrinho);
+
+        assertEquals(1500.0, valor, 0.0001);
+    }
+
 }
